ea69f7209b22ecf5e477fd5d6a8fcb63e7c87524
anaisabelesquivel11-sketch <anaisabelesquivel11@gmail.com>
2025-08-12 23:24:16 -0600

    Add files via upload

--- Semana2/Ago12_ej1.c
+++ Semana2/Ago12_ej1.c
@@ -1,28 +1,34 @@
-//Ejercicio de clase 12 de agosto
-//Repaso de ciclos for, errores de redondedo, jerarquía de operaciones y condicionalo if and else
-
-
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 
 int main()
 {
-  int num[5]={1,2,3,4,5};
-  int tamaño= sizeof(num)/sizeof(num[0]);
+  int numeros[5];
   float resultados[5];
-  for (int i=0; i<tamaño; i++)
-  {
-  //printf("%d %d\n", num[i], num[i]%2);
-  if(num[i]%2==0)
-  {
-  resultados[i]=log10(num[i]+1);
-  printf("x= %i\n log10(x+1)=%.2f\n ", num[i], resultados[i]);
-  }
-  else{
-    resultados[i]=exp(num[i])+cos(num[i]);
-    printf("x= %i\n exp(x)+cos(x)=%.2f\n", num[i], resultados[i]);
-  }
-  }
-  printf("Pi + Pi= %lf", 3.1415926536+3.1415926536);
-  return 0;
+  int indx;
+  float pi= 3.1415926536;
+
+  numeros[0]= 1;
+  numeros[1]= 2;
+  numeros[2]= 3;
+  numeros[3]= 4;
+  numeros[4]= 5;
+//printf("x\t x^2 \t log(x+1) \t exp(x)+cos(x)\n");
+
+for(indx=0; indx<5;indx++){
+  //printf("%d %d\n", numeros[indx], numeros[indx]%2);
+  if(numeros[indx]%2==0){
+    resultados[indx]=log10(numeros[indx]+1);
+    printf(" x%d \n log(x+1)= %f\n", numeros[indx], resultados[indx]);
+  }else{
+    resultados[indx]=exp(numeros[indx])+cos(numeros[indx]);
+printf(" x=%d \n exp(x)+cos(x)= %f\n", numeros[indx],resultados[indx]);
+}
+}
+printf("Resultados\n");
+for(indx=0; indx<5;indx++)
+{
+  printf("%f\n",resultados[indx]);
+}
+printf("Pi+Pi=%.16lf", pi+pi);
 }
