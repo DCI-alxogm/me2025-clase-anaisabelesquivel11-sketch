3f0809a548d5b35b02a7042a101dac461250ae64
anaisabelesquivel11-sketch <anaisabelesquivel11@gmail.com>
2025-08-15 11:56:44 -0600

    Add files via upload

---
+++ Semana2/TareaSemana2_ej1.c
@@ -0,0 +1,19 @@
+//Ana Isabel Esquivel Castro, 436578
+//Tarea 1 Semana 2: realizar código de soluciones cuadráticas
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  float a,b,c, dsrc, x1, x2, xi1, xi2;
+  printf("Ingrese valores para a: , b: y c: ");
+    scanf("%f %f %f", &a, &b,&c);
+
+    if ()//definir a==0
+    if b==0 //definir b !=0; 
+    printf("Al ingresar valores para a y b = 0, obtenemos la solución trivial");
+    if ()
+
+ 
+}
